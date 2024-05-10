98d135fe01ee11abbd7d7f4309a56fc9776443da
Thang <107343290+ThangBuiChien@users.noreply.github.com>
2024-05-10 09:59:43 +0700

    no special ban keyword for searching

--- src/main/java/auction/controllers/URLFIlter.java
+++ src/main/java/auction/controllers/URLFIlter.java
@@ -18,7 +18,9 @@ import javax.servlet.annotation.WebFilter;
 
 public class URLFIlter implements Filter {
     private static final Pattern SQL_INJECTION_PATTERN = Pattern.compile(".*(\\bunion\\b|\\bselect\\b|\\binsert\\b|\\bdelete\\b|\\bupdate\\b).*", Pattern.CASE_INSENSITIVE);
-    private static final Pattern BANNED_CHARACTERS_PATTERN = Pattern.compile("[&<>'\";\\s\\x00-\\x1F]");
+    //private static final Pattern BANNED_CHARACTERS_PATTERN = Pattern.compile("[&<>'\";\\s\\x00-\\x1F]");
+    private static final Pattern BANNED_CHARACTERS_PATTERN = Pattern.compile("@");
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // Initialization code
